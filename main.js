c9065962746b1f9f15cc7a9681a61c33b7bf85b2
Sasha Raimov <crazyysasha@gmail.com>
2023-05-20 18:52:22 +0500

    fix

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
 import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter, myFunction } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="increment" type="button">increment</button>
+      <button id="decrement" type="button">decrement</button>
+      <div id="output"></div>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`
+
+setupCounter(increment, decrement, output)
 
 
-const button = document.getElementById("button");
-
-
-button.addEventListener('click', function (event) {
-  output.innerHTML = "clicked!";
-  primer.innerHTML = "sfghfgds";
-});
+myFunction();
