07740d0d33f34f248e72711392a9754ee4c62fb8
Sasha Raimov <crazyysasha@gmail.com>
2023-05-20 18:10:11 +0500

    fix

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 import './style.css'
+
+
+const button = document.getElementById("button");
+
+
+button.addEventListener('click', function (event) {
+  output.innerHTML = "clicked!";
+  primer.innerHTML = "sfghfgds";
+});
